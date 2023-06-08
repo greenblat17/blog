c43799f99a1a0183788fb8cc6e3d3960bb694d9a
Alex Zhuravlev <leftemail17@mail.ru>
2023-06-08 13:53:40 +0300

    adding user (admin)

---
+++ src/main/java/com/greenblat/blog/model/User.java
@@ -0,0 +1,26 @@
+package com.greenblat.blog.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@EqualsAndHashCode(of = "username")
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "usr")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+}
